356d0469002322a6ce43c6c56a789795e3d6c6a5
noah-00 <t71.ishizaka.naoya@gmail.com>
2025-06-14 17:45:42 -0700

    feature: add messages curd

---
+++ convex/messages.ts
@@ -0,0 +1,42 @@
+import { v } from "convex/values";
+import { mutation, query } from "./_generated/server";
+
+export const getAll = query({
+	handler: async (ctx) => {
+		const identity = await ctx.auth.getUserIdentity();
+		if (!identity) {
+			throw new Error("UnAuthorized");
+		}
+		const userId = identity.subject;
+
+		const messages = await ctx.db
+			.query("messages")
+			.withIndex("by_user", (q) => q.eq("userId", userId))
+			.order("desc")
+			.collect();
+
+		return messages;
+	},
+});
+
+export const create = mutation({
+	args: {
+		title: v.string(),
+		content: v.optional(v.string()),
+	},
+	handler: async (ctx, args) => {
+		const identity = await ctx.auth.getUserIdentity();
+		if (!identity) {
+			throw new Error("UnAuthorized");
+		}
+		const userId = identity.subject;
+
+		const message = await ctx.db.insert("messages", {
+			title: args.title,
+			content: args.content,
+			userId,
+		});
+
+		return message;
+	},
+});
